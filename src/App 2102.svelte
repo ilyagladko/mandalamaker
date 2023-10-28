2b58f7ef7e4a7c620ede48a5f42712469bd85f8c
ilya gladko <ilyagladko@ilyas-MacBook-Air.local>
2023-10-28 18:34:47 +0300

    sec comit

---
+++ src/App 2102.svelte
@@ -0,0 +1,229 @@
+БыскшзеЮ
+<script>
+	import P5 from 'p5-svelte';
+	import { saveAs } from 'file-saver';
+	import Range from "./Range.svelte"	
+	
+	let c = "";
+
+	// let h1 = 0;
+	// let s1 = 130;
+	// let b1 = 130;
+	let h1 = Math.ceil(Math.random()*255);
+	let s1 = Math.ceil(Math.random()*255);
+	let b1 = Math.ceil(Math.random()*55)+200;
+
+	let h2 = 0;
+	let s2 = 50;
+	let b2 = 130;
+
+	let wWidth = window.innerWidth;
+	let wHeight = window.innerHeight;
+	let bgcolor = "black";	
+	// let time = 0;
+
+	let quantity = Math.ceil(Math.random()*10)+2;
+	let ellipses = Math.ceil(Math.random()*8)+1;
+	let ellipseX = 150;
+	let ellipseY = 275;
+	let offsetX = -20;
+	let offsetY = 10;
+	let w = 15;
+	$: weight = w/10;
+	let amp = 1;
+
+	
+	const sketch = (p5) => {
+		p5.setup = () => {
+			c = p5.createCanvas(wWidth, wHeight);
+			p5.colorMode(p5.HSB, 255);
+			p5.smooth();		
+			
+			p5.background(bgcolor);
+			console.log(p5.colorMode);
+
+			console.log(h1, s1, b1);
+			
+			let col1 = p5.color(h1, s1, b1);
+			let col2 = p5.color(h2, s2, b2);
+
+		p5.draw = () => {
+		p5.noStroke();  
+		p5.fill(bgcolor);
+		p5.rect(0,0,wWidth,wHeight);
+
+		let sineX = Math.sin(h1) * amp;
+		let cosY = Math.cos(h1) * amp;
+
+		let angrot = 0.0;
+		let angrotinc = 180.0/quantity;
+  
+		let mellipseY = ellipseY;
+		if (offsetY > 0) { mellipseY = ellipseY + (ellipses * offsetY); }
+	  
+		let mscale = (wHeight) / mellipseY;
+  
+		p5.noFill();
+		p5.strokeWeight(weight);
+
+		col1 = p5.color(h1, s1, b1);
+		col2 = p5.color(h2, s2, b2);
+
+		for (let j = 0; j < quantity; j++) {
+			  ellps((ellipseX*mscale),(ellipseY*mscale),ellipses,(offsetX*mscale),(offsetY*mscale),angrot)
+			//   ellps(((ellipseX + sineX)*mscale),((ellipseY + cosY)*mscale),ellipses,(offsetX*mscale),(offsetY*mscale),angrot)
+			  angrot = angrot + angrotinc;
+		}
+
+		function ellps (radx,rady,num,radoffsetX,radoffsetY,ellprot ) {
+			p5.angleMode(p5.DEGREES); 
+			let nradx = radx;
+			let nrady = rady;
+  
+			  for (let i = 0; i < num; i++) {
+				  p5.stroke(p5.lerpColor(col1,col2,(i/num)));
+				  p5.push();
+				  p5.translate(wWidth*0.5,wHeight*0.5);
+				  p5.rotate(ellprot);
+				  p5.ellipse(0,0,nradx,nrady);
+		  
+				  p5.pop();
+  
+				  nradx = nradx + radoffsetX;
+				  nrady = nrady + radoffsetY;
+				}
+				
+		  }
+	  }
+
+    }
+}
+
+	function saveImage (){
+		// var link = document.createElement('a');
+		// link.download = 'mandala'+Math.round(Math.random()*9999)+'.png';
+		// link.href = document.getElementById('defaultCanvas0').toDataURL()
+		// link.click();
+
+		// const dataUrl = document.getElementById('defaultCanvas0').toDataURL("png");
+  		// console.log(dataUrl);
+  		// const win = window.open(dataUrl, '_blank');
+		// window.open(document.getElementById('defaultCanvas0').toDataURL("image/png"), '_blank');
+
+		var canvas = document.getElementById("defaultCanvas0");
+			canvas.toBlob(function(blob) {
+    		saveAs(blob, 'mandala'+Math.round(Math.random()*9999)+'.png');
+		});
+
+	}
+
+	function randomize () {
+		quantity = Math.ceil(Math.random()*10)+2;
+		ellipses = Math.ceil(Math.random()*8)+1;
+		ellipseX = Math.ceil(Math.random()*200);
+		ellipseY = Math.ceil(Math.random()*200);
+		offsetX = Math.ceil((Math.random()*300)-100);
+		offsetY = Math.ceil((Math.random()*300)-100);
+		weight = (Math.random()*2)+0.5;
+
+		h1 = Math.ceil(Math.random()*256);
+		s1 = Math.ceil(Math.random()*156)+100;
+		b1 = Math.ceil(Math.random()*156)+100;
+
+		h2 = Math.ceil(Math.random()*256);
+		s2 = Math.ceil(Math.random()*156)+100;
+		b2 = Math.ceil(Math.random()*156)+100;
+	}
+
+
+</script>
+	
+<div class="controls">
+	<div class="row">
+		<Range bind:value={quantity} min={1} max={30} />
+	</div>
+	<div class="row">
+		<Range bind:value={ellipses} min={1} max={25} />
+	</div>
+	<div class="row">
+		<Range bind:value={ellipseX} min={50} max={500} />
+	</div>
+	<div class="row">
+		<Range bind:value={ellipseY} min={1} max={300} />
+	</div>
+	<div class="row">
+		<Range bind:value={offsetX} min={-100} max={300} />
+	</div>
+	<div class="row">
+		<Range bind:value={offsetY} min={-100} max={300} />
+	</div>
+	<div class="row">
+		<Range bind:value={w} min={5} max={30} />
+	</div>
+	<div class="row">
+		<Range bind:value={h2} min={1} max={254} />
+		<Range bind:value={s2} min={1} max={254} />
+		<Range bind:value={b2} min={1} max={254} />
+	</div>
+	<div class="row">
+		<Range bind:value={h1} min={1} max={254} />
+		<Range bind:value={s1} min={1} max={254} />
+		<Range bind:value={b1} min={1} max={254} />
+	</div>
+	<div class="btns">
+		<button class="butt" on:click={randomize}>random</button>
+		<button class="butt" on:click={saveImage}>save</button>
+	</div>
+</div>
+
+<div class="container">
+	<P5 {sketch} />
+</div>
+
+<style>
+	:global(html, body) {
+		margin: 0;
+		padding: 0;
+		width: 100%;
+		height: 100%;
+		background-color: black;
+		-webkit-font-smoothing: antialiased;
+	}
+	.controls {
+		display: flex;
+		position: fixed;
+		flex-direction: column;
+		justify-content: stretch;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		z-index: 99;
+	}
+	
+	.row {
+		display: flex;
+		flex-direction: row;
+		height: 100%;
+	}
+	
+	.container {
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		z-index: 0;
+	}
+
+	.butt {
+		font-size: 12px;
+		line-height: 20px;
+		background:transparent;
+		color: white;
+		border: 0;
+	  }
+</style>
---
+++ src/App-works.svelte
@@ -0,0 +1,363 @@
+<script>
+	import P5 from 'p5-svelte';
+  
+	  var nrepeat = 7;
+	  var nellps = 6;
+	  var elpx = 150;
+	  var elpy = 275;
+	  var xinc = -20;
+	  var yinc = 10;
+
+	  let colorA = 'white';
+	  let colorB = 'white';
+  
+	  var pwidth = window.innerWidth;
+	  var pheight = window.innerHeight;
+	  var xcen = pwidth*0.5;
+	  var ycen = pheight*0.5;
+  
+	  let opacity = 0.9;
+	//   var bgcolor = "black"
+	  var bgcolor = "black";
+	  var weight = 1;
+	  let time = 0;
+	  let speed = 200;
+	  let amp = 1;
+	//   let mix = 0;
+	//   let buttonColor;
+	//   let buttonRandom;
+	  
+	  
+	  // let colorA = 'white'
+	  // let colorB = 'blue'
+	  
+	  function getRandomColor() {
+		  var letters = '0123456789ABCDEF';
+		  var color = '#';
+		  for (var i = 0; i < 6; i++) {
+			  color += letters[Math.floor(Math.random() * 16)];
+			}
+			return color;
+		}
+
+
+			const randomize = () => {
+				nrepeat = Math.random()*8;
+				nellps = Math.random()*10;
+				elpx = Math.random()*200;
+				elpy = Math.random()*200;
+				xinc = Math.random()*(300-100);
+				yinc = Math.random()*(300-100);
+				opacity = Math.random()*1;
+				weight = (Math.random()*1)+0.2;
+
+				// colorA = p5.getRandomColor();
+				// colorB = p5.getRandomColor();
+
+				// reColor();
+			}
+
+		const sketch = (p5) => {
+
+			// let div = p5.createDiv('Hello ').size(100, 20);
+			// div.html('World', true);
+			
+			const reColor = () => {
+				colorA = p5.color(getRandomColor());
+				colorB = p5.color(getRandomColor());
+				
+				console.log(colorA+" "+colorB);
+				}
+
+
+				
+			p5.setup = () => {
+				let c = p5.createCanvas(pwidth, pheight);
+				p5.smooth();
+				
+				// buttonColor = p5.createButton('recolor');
+				// buttonColor.position(20, 20);
+  				// buttonColor.mousePressed(reColor);
+
+				// buttonRandom = p5.createButton('random');
+				// buttonRandom.position(100, 20);
+  				// buttonRandom.mousePressed(randomize);
+
+		  p5.background(bgcolor);
+	  };
+  
+	//   colorA = p5.color(getRandomColor());
+	colorA = p5.color('white');
+	colorB = p5.color(getRandomColor());
+	
+	p5.draw = () => { 
+		time += speed;
+		if(time>1000){time -= 1000;}
+		p5.noStroke();  
+		p5.fill(bgcolor); 
+		p5.rect(0,0,pwidth,pheight);
+		let sineX = Math.sin(time) * amp;
+		let cosY = Math.cos(time) * amp;
+		p5.colorMode('HSB')
+		var col = p5.color(time,255,0);
+			
+			// let mixTarget = p5.map(p5.mouseX, 0, pwidth, 0.0, 1.0);
+	  		// mix = mix + ((mixTarget - mix));
+			// console.log(mix);
+
+		let angrot = 0.0;
+		let angrotinc = 180.0/nrepeat;
+  
+		  let melpy = elpy;
+			if (yinc > 0) { melpy = elpy + (nellps * yinc); }
+	  
+		  let mscale = (pheight) / melpy;
+  
+		  p5.noFill();
+		  p5.strokeWeight(weight);
+		//  p5.stroke('rgba(255,255,255,'+(opacity)+')');
+
+		  for (let j = 0; j < nrepeat; j++) {
+			  ellps(((elpx + sineX)*mscale),((elpy + cosY)*mscale),nellps,(xinc*mscale),(yinc*mscale),angrot)
+			  angrot = angrot + angrotinc;
+		  }
+		  
+		function ellps (radx,rady,num,radxinc,radyinc,ellprot ) {
+			p5.colorMode('RGB')
+			p5.angleMode(p5.DEGREES); 
+			let nradx = radx;
+			let nrady = rady;
+			// const colorA = p5.color('red');
+			// const colorB = p5.color('#344231');
+			// console.log (colorA);
+			// const colorB = Math.floor(Math.random()*16777215).toString(16);
+  
+			  for (let i = 0; i < num; i++) {
+				  p5.stroke(p5.lerpColor(col,colorB,(i/num)));
+				//   p5.stroke('white');
+				  p5.push();
+				  p5.translate(xcen,ycen);
+				  p5.rotate(ellprot);
+				  p5.ellipse(0,0,nradx,nrady);
+		  
+				  p5.pop();
+  
+				  nradx = nradx + radxinc;
+				  nrady = nrady + radyinc;
+				}
+				
+		  }
+	  }
+}
+
+
+function saveImage (){
+  var link = document.createElement('a');
+  link.download = 'mandala'+Math.round(Math.random()*9999)+'.png';
+  link.href = document.getElementById('defaultCanvas0').toDataURL()
+  link.click();
+}
+
+//   function savePNG (p5) {
+//   	//let pngname = str(nf(month(),2))+str(nf(day(),2))+str(year())+"_"+str(nf(hour(),2))+str(nf(minute(),2))+str(nf(second(),2));
+
+// 	  photo = loadImage('assets/rockies.jpg');
+// }
+
+// function draw() {
+//   p5.image(photo, 0, 0);
+// }
+
+// function keyTyped() {
+//   if (key === 's') {
+//     p5photo.save('photo', 'png');
+//   }
+// }
+
+//   	let newfile = "Mandala";
+//   	let img = p5.get(0,0,pwidth,pheight);
+//   	p5.save(newfile, 'png');
+//   	setTimeout(4000);
+//   }
+  </script>
+  
+  <div class="controls">
+	  <div class="row">
+		  <p>Ellipses {nrepeat}</p>
+			  <input type="range" bind:value={nrepeat} min="1" max="50" step="1" />
+	  </div>
+	  <div class="row">
+		  <p>Repeat {nellps}</p>
+			  <input type="range" bind:value={nellps} min="1" max="20" step="1" />
+	  </div>
+	  <div class="row">
+		  <p>Radius X {elpx}</p>
+			  <input type="range" class="slider" bind:value={elpx} min="100" max="600" step="1" />
+	  </div>
+	  <div class="row">
+		  <p>Radius Y {elpy}</p>
+			  <input type="range" class="slider" bind:value={elpy} min="1" max="300" step="1" />
+	  </div>
+	  <div class="row">
+		  <p>Indent X {xinc}</p>
+			  <input type="range" class="slider" bind:value={xinc} min="-100" max="300" step="1" />
+	  </div>
+	  <div class="row">
+		  <p>Indent Y {yinc}</p>
+			  <input type="range" class="slider" bind:value={yinc} min="-100" max="300" step="1" />
+	  </div>
+	  <!-- <div class="row">
+		  <p>Opacity {opacity}</p>
+			  <input type="range" class="slider" bind:value={opacity} min="0.1" max="1" step="0.05" />
+	  </div> -->
+	  <div class="row">
+		  <p>Weight {weight}</p>
+			  <input type="range" class="slider" bind:value={weight} min="0.1" max="4" step="0.05" />
+	  </div>
+	  <div class="row">
+		<button class="butt" on:click={saveImage}>save</button>
+		<button class="butt" on:click={randomize}>random</button>
+	</div>
+  </div>
+  
+  <div class="container">
+	  <P5 {sketch} />
+  </div>
+  
+  <style>
+	  :global(html, body) {
+		  margin: 0;
+		  padding: 0;
+		  width: 100%;
+		  height: 100%;
+		  background-color: black;
+		  -webkit-font-smoothing: antialiased;
+	  }
+	  .container {
+		  display: flex;
+		  align-items: center;
+		  justify-content: center;
+  }
+  .controls {
+	/* display: none; */
+	  position: fixed;
+	  display: flex;
+	  flex-direction: column;
+	  justify-content: space-evenly;
+	  /* align-items: end; */
+	  /* right: 20px; */
+	  /* bottom: 10px; */
+	  left: 20px;
+	  z-index: 9;
+	  height: calc(100vh - 80px);
+	}
+	.row {
+		  /* width: calc(100vw-40px); */
+		  display: flex;
+		  flex-direction: row;
+		  /* height: 100px; */
+		  /* width: calc(100vw - 40px); */
+		  align-items: center;
+		  justify-content: right;
+		  /* gap: 54px; */
+	  }
+	  .butt {
+		font-size: 12px;
+		line-height: 20px;
+		background:transparent;
+		color: white;
+		border: 0;
+		/* text-color */
+	  }
+	  p {
+		  color: rgba(255,255,255,0.6);
+		  font-size: 12px;
+		  line-height: 20px;
+		  display: none;
+	  }
+	  input[type="range"] {
+		  -webkit-appearance: none;
+		  -moz-appearance: none;
+		  appearance: none;
+		  border: 0;
+		  background: transparent;
+		  /* background: black; */
+		  /* width: 250px; */
+		  width: calc(100vw - 40px);
+		  height: calc((100vh - 40px)/8);
+		  /* outline: 0; */
+		  cursor: grab;
+	  }
+  
+	  input[type="range"]::-webkit-slider-thumb {
+		  -webkit-appearance: none;
+		  appearance: none;
+		  /* background-color: rgba(255,255,255,0.8); */
+		  background-color: white;
+		  /* background-color: transparent; */
+		  /* border-color: white; */
+		  width: 8px;
+		  height: 8px;
+		  border-radius: 50%;
+		  cursor: grab;
+		  transition: .3s ease-out;
+		}
+		
+		input[type="range"]::-webkit-slider-thumb:hover {
+			background-color: rgba(255,255,255,1);
+			transform: scale(1.3);
+		}
+	  
+	  input[type="range"]::-webkit-slider-thumb:active {
+	  transform: scale(1.4);
+	}
+
+	input[type=range]::-ms-track {
+    width: 80px;
+    height: 80px;
+    
+    /*remove bg colour from the track, we'll use ms-fill-lower and ms-fill-upper instead */
+    background: transparent;
+    
+    /*leave room for the larger thumb to overflow with a transparent border */
+    border-color: transparent;
+    border-width: 6px 0;
+
+    /*remove default tick marks*/
+    color: transparent;
+}
+  
+  @media only screen and (max-width: 1000px) {
+	  .row {
+		  /* height: 100px; */
+		  gap: 16px;
+	  }
+	  p {
+		display: none;
+	  }
+	  .controls {
+		width: 100%;
+		  z-index: 9;
+	  }
+	  /* input[type="range"] {
+		  -webkit-appearance: none;
+		  -moz-appearance: none;
+		  appearance: none;
+		  border: 0;
+		  background: transparent;
+		  height: 100px;
+		  width: 600px;
+		  outline: 0;
+		  cursor: pointer;
+	  } 
+	  */
+  
+	  input[type="range"]::-webkit-slider-thumb {
+		  /* width: 4px; */
+		  /* height: 4px; */
+		  border-radius: 50%;
+		  cursor: grab;
+		  transition: .3s ease-out;
+	  }
+  }
+  </style>
